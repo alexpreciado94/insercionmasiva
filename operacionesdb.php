4bdac670a9b0247958361268e51999174ac6597f
alexpreciado94 <alexpreciado94@hotmail.com>
2022-01-13 21:21:54 +0100

    $$$

--- operacionesdb.php
+++ operacionesdb.php
@@ -3,7 +3,6 @@
     function __construct(){
       include_once 'configdb.php';
       $this->conexion = new mysqli(SERVIDORDB, USUARIO, CONTRASENA, BASEDATOS);
-      //$this->conexion = new mysqli('localhost', 'root', '', 'empleadosphp');
     }
     function consultar($consulta){
       $resultado = mysqli_query($this->conexion, $consulta);
